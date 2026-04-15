250c8ad2f135a3cfb0d8adffab896d1d00e89f1a
Manish <manishwaghmare8080@gmail.com>
2026-04-15 18:59:59 +0530

    Renamed class

--- src/test/java/amazontestscripts/SearchMobtest.java
+++ src/test/java/amazontestscripts/SearchMobtest.java
@@ -21,7 +21,7 @@ import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class SearchMobtest {
+public class SearchMobTest {
 	@Test
 	public void searchMobile() throws InterruptedException {
 
